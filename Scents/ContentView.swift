175bb2495c1e3da443e92564a7c4eaf55ef82672
Mert Bildirici <hmertbildirici@gmail.com>
2024-04-15 21:25:40 -0400

    mertt

--- Scents/ContentView.swift
+++ Scents/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, mer6!")
+            Text("Hello, Mert!")
         }
         .padding()
     }
---
+++ Scents/Discover.swift
@@ -0,0 +1,10 @@
+//
+//  Discover.swift
+//  Scents
+//
+//  Created by Mert Bildirici on 4/15/24.
+//
+
+import Foundation
+
+
