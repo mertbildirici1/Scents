b2a6d6a97b01135fe79689989a896a26d979eaae
Mert Bildirici <hmertbildirici@gmail.com>
2024-04-15 21:19:01 -0400

    Initial Commit

---
+++ Scents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scents
+//
+//  Created by Mert Bildirici on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scents/ScentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScentsApp.swift
+//  Scents
+//
+//  Created by Mert Bildirici on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ScentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
